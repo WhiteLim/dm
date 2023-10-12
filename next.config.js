f798a62429b2ac88463befc5105b8ddc9fe44524
WhiteLim <eunhan09@naver.com>
2023-10-12 16:01:28 +0900

    test:test

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const withPWA = require('next-pwa')({
     // disable: process.env.NODE_ENV === 'development',
     // register: true,
     // scope: '/app',
-    //sw: 'sw.js'
+    sw: 'sw.js'
     //...
   })
   
