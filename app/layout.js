46cffd4c80987f7f1bf0f82a282f2ad29738b1eb
WhiteLim <eunhan09@naver.com>
2023-10-12 13:11:03 +0900

    test & updata : pwa 설정 테스트 & 인트로 업데이트

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Heads from './Heads'
 import './globals.scss'
 import { Inter } from 'next/font/google'
 
@@ -12,27 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="apple-touch-icon" sizes="57x57" href="/fa/apple-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="60x60" href="/fa/apple-icon-60x60.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="/fa/apple-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/fa/apple-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="/fa/apple-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="/fa/apple-icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="/fa/apple-icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/fa/apple-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/fa/apple-icon-180x180.png" />
-        <link rel="icon" type="image/png" sizes="192x192"  href="/fa/android-icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/fa/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/fa/favicon-96x96.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/fa/favicon-16x16.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-TileImage" content="/fa/ms-icon-144x144.png" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="viewport" content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width" />
-        <link rel='manifest' href='/manifest.json' />
-      </Head>
+      <Heads />
       <body className={inter.className}>{children}</body>
     </html>
   )
--- app/page.js
+++ app/page.js
@@ -1,51 +1,89 @@
 "use client"
-import { useEffect, useState } from "react";
-import Kakao from "./pages/Kakao";
-import axios from "axios";
+import React, { useEffect, useRef, useState } from 'react'
+import style from './pages/Intro/page.module.scss'
+import KakaoLogin from 'react-kakao-login';
+
+export default function page() {
+  const [loading, setLoading] = useState(true);
+  const loadingimg = useRef();
+
+  useEffect(() => {
+    //로딩 화살표 ing
+    //const loadingimg = document.querySelector('.loading');
+
+    for (let i = 0; i < 8; i++) {
+      loadingimg.current.innerHTML += "<img src='/img/intro/vector (1).png'>"
+    }
 
 
-export default function Home() {
+    const loadImg2 = loadingimg.current.childNodes;
+    loadImg2.forEach((v, k) => {
+      setTimeout(() => {
+        v.src = "/img/intro/vector.png"
+      }, 200 * k)
+    }, []);
 
-  // 리스트용 변수
-  const [list,setList] = useState();
-  const [page,setPage] = useState(10);
 
-  // 디테일용 변수
-  const [detail,setDetail] = useState(10);
+    //로딩이 끝났을 때 바뀌는 화면
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
 
-  // 디지몬 리스트
+  const handleKakaoLogin =async (response) => {
+    const u_id = response.profile.id;
+    const u_nick = response.profile.properties.nickname;
+    const d = await axios.post('/api/member',{u_id,u_nick})
+    console.log(d.data);
+    if(d.data[0] == false) {
+      console.log("회원가입하셈"+d.data[1]);
+    } else {
+      console.log("이미 회원이셈");
+    }
+  };
 
-  async function listget(){
-    const res = await axios.get('/api/dex/list');
-    setList(res.data.content);
-  }
-  
-  // 디지몬 디테일
-  async function detailget(){
-    const res = await axios.get('/api/dex/detail?num=100');
-    setDetail(res.data);
-  }
 
-  useEffect(()=>{
-    listget()
-    detailget();
-  },[])
+
 
 
   return (
-    <>
-    <video autoPlay muted loop playsInline src='/img/background.mp4' />
-     <Kakao />
-      {
-        list && list.slice(0, page).map(v=>(
-          <p key={v.id}>{v.name}</p>
-        ))
-      }
-      <button onClick={()=>{ setPage(page + 10)  }}>더보기</button>
-
-      <div>
-        <p>{detail.name}</p>
-      </div>
-    </>
+    <article className={style.main_intro}>
+      <video className={style.bg_video} autoPlay muted loop playsInline src='/img/background.mp4'/>
+      <figure className={style.main_logo}>
+        <img src="/img/intro/Group 2.png" />
+        {loading ? (
+          <figcaption className={style.loadfirst}>
+            <div className={style.loading} ref={loadingimg}></div>
+            <p>디지털 월드에 접속중 ...</p>
+            <img src="/img/intro/intro.gif" />
+          </figcaption>
+        ) : (
+          <section className={style.loadlast}>
+            <div>
+              <img src="/img/intro/WELCOME TO DIGITAL WORLD.png" alt="" />
+            <KakaoLogin
+                token={process.env.NEXT_PUBLIC_KAKAO_JS_KEY}
+                onSuccess={handleKakaoLogin}
+                onFail={console.error}
+                onLogout={console.info}
+                render={({ onClick }) => {
+                  return (
+                    <a
+                      href="#"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        onClick();
+                      }}
+                    >
+                      <img src="/img/intro/Group 181.png" alt='' />
+                    </a>
+                  );
+                }}
+              /> 
+            </div>
+          </section>
+        )}
+      </figure>
+    </article>
   )
 }
