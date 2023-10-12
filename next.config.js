82c165301e96ba620e61a8eaaf441f9931524c2b
WhiteLim <eunhan09@naver.com>
2023-10-12 17:42:09 +0900

    update : PWA 완료

--- next.config.js
+++ next.config.js
@@ -1,23 +1,39 @@
-
-const withPWA = require("next-pwa");
-const withPlugins = require("next-compose-plugins");
+const path = require("path");
+const withPWAInit = require("next-pwa");
 
 const nextConfig = {
-    reactStrictMode: false,
-}
+    reactStrictMode: true,
+    webpack: (config) => {
+      const entry = generateAppDirEntry(config.entry);
+      config.entry = () => entry;
+  
+      return config;
+    },
+  };
+  
 
-module.exports = withPlugins(
-	[
-		[
-			withPWA,
-			{
-				pwa: {
-					dest: "public",
-                    sw: 'sw.js',
-				},
-			},
-		],
-		// 추가 플러그인 작성
-	],
-	nextConfig
-);
+const withPWA = withPWAInit({
+  dest: "public",
+  buildExcludes: ["app-build-manifest.json"],
+});
+
+const generateAppDirEntry = (entry) => {
+  const packagePath = require.resolve('next-pwa');
+  const packageDirectory = path.dirname(packagePath);
+  const registerJs = path.join(packageDirectory, "register.js");
+
+  return entry().then((entries) => {
+    if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
+      if (Array.isArray(entries["main-app"])) {
+        entries["main-app"].unshift(registerJs);
+      } else if (typeof entries["main-app"] === "string") {
+        entries["main-app"] = [registerJs, entries["main-app"]];
+      }
+    }
+    return entries;
+  });
+};
+
+
+
+module.exports = withPWA(nextConfig);
