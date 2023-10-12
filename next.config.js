da2b593010c9a3236fbb5b140bb77975cbf8cac6
WhiteLim <eunhan09@naver.com>
2023-10-12 15:27:26 +0900

    test:pwa 다시 테스트

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa');
+const withPlugins = require("next-compose-plugins");
+const withPWA = require("next-pwa");
 const nextConfig = {
     reactStrictMode: false,
 }
 
-module.exports = nextConfig
+module.exports = withPlugins(
+	[
+		[
+			withPWA,
+			{
+				pwa: {
+					dest: "public",
+				},
+			},
+		],
+		// 추가 플러그인 작성
+	],
+	nextConfig
+);
