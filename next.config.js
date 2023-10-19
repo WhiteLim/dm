a9f8794c0e1aa7bba0e666e280f8660dfe6cf26c
WhiteLim <eunhan09@naver.com>
2023-10-19 17:45:35 +0900

    update : join id 비활성화 / PWA 적용

--- next.config.js
+++ next.config.js
@@ -1,26 +1,26 @@
-/** @type {import('next').NextConfig} */
-const withPlugins = require("next-compose-plugins");
-const withPWA = require("next-pwa");
-const nextConfig = {
-    reactStrictMode: false,
-}
+// /** @type {import('next').NextConfig} */
+// const withPlugins = require("next-compose-plugins");
+// const withPWA = require("next-pwa");
+// const nextConfig = {
+//     reactStrictMode: false,
+// }
 
-module.exports = withPlugins(
-	[
-		[
-			withPWA,
-			{
-				pwa: {
-					dest: "public",
-				},
-			},
-		],
-		// 추가 플러그인 작성
-	],
-	nextConfig
-);
+// module.exports = withPlugins(
+// 	[
+// 		[
+// 			withPWA,
+// 			{
+// 				pwa: {
+// 					dest: "public",
+// 				},
+// 			},
+// 		],
+// 		// 추가 플러그인 작성
+// 	],
+// 	nextConfig
+// );
 
-/* 
+
 
 const path = require("path");
 const withPWAInit = require("next-pwa");
@@ -60,4 +60,4 @@ const generateAppDirEntry = (entry) => {
 
 
 
-module.exports = withPWA(nextConfig); */
+module.exports = withPWA(nextConfig);
